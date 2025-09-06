13196eea9380079f55dacdb410fd89fcb61d3e8f
Santhosh-p653 <santhosh11042007@gmail.com>
2025-09-06 17:55:50 +0530

    Added data and scripts directories for analysis handling

---
+++ scripts/analyze_raw.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+file_path = '/content/drive/My Drive/Colab Notebooks/geophone.csv.xlsx'
+df = pd.read_excel(file_path)
+print(df.head())
+df['timestamp'] = pd.to_datetime(df['timestamp'], format='%H:%M:%S')
+
+plt.figure(figsize=(12, 6))
+plt.plot(df['timestamp'], df['dominant_freq'])
+plt.xlabel('Timestamp')
+plt.ylabel('Dominant Frequency')
+plt.title('Dominant Frequency Over Time')
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+plt.gcf().autofmt_xdate()
+plt.show()
