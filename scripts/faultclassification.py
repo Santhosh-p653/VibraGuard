24df675fbd70949262422f4bea68e0fc8f1fab81
Santhosh-p653 <santhosh11042007@gmail.com>
2025-09-09 23:13:45 +0530

    Update faultclassification.py

--- scripts/faultclassification.py
+++ scripts/faultclassification.py
@@ -2,11 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import config
 
-# --- Load and prepare data ---
-file_path = r'c:\vibraguard\data\geophone-sensor-data.xlsx'
-
-df = pd.read_excel(file_path)
+df = pd.read_excel(config.x)
 
 df['timestamp'] = pd.to_datetime('2000-01-01 ' + df['timestamp'].astype(str), errors='coerce')
 df = df.dropna(subset=['timestamp'])
